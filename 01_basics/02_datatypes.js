6055d3db0884d453693a0e3f9bb36f93f3b7415e
Aryan Rustagi <114554073+aryanrustagi@users.noreply.github.com>
2025-02-19 06:35:25 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+// alert(3+3)
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Aryan")
+
+
+
+let name = "Aryan"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
