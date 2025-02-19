e2781811625eda5ed37a2aa17f0f0cf1efcf93a7
Aryan Rustagi <114554073+aryanrustagi@users.noreply.github.com>
2025-02-19 07:58:03 +0000

    datatype summary

---
+++ datatype_summary.js
@@ -0,0 +1,35 @@
+// Premitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 1758927362483278519647586348751n
+
+
+
+// Reference (Non Premitive)
+
+// Array, Objects, Functions
+
+const heros = ["krish", "shaktiman"];
+let myObj = {
+    name: "Aryan",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
